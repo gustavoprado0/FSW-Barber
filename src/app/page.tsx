18376da5153c6a7f6ae22c49ef7b849903e4a6f4
Gustavo Prado <gustavopsz2002@gmail.com>
2024-08-11 22:09:37 -0300

    feat: add recomn recommended and popular with datebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,93 @@
-'use client'
+import { Button } from "./_components/ui/button"
+import Image from "next/image"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
+import BookingItem from "./_components/booking-item"
+import Link from "next/link"
+import Header from "./_components/ui/header"
+import { Search } from "lucide-react"
+import { quickSearchOptions } from "./constants/search"
+import { Input } from "./_components/ui/input"
 
-import { SearchIcon } from "lucide-react";
-import Header from "./_components/ui/header";
-import { Input } from "./_components/ui/input";
-import { Button } from "./_components/ui/button";
-import Image from "next/image";
-import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar } from "./_components/ui/avatar";
-import { AvatarImage } from "@radix-ui/react-avatar";
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
 
-const Home = () => {
-  return ( 
-    //Header
-    <>
+  return (
+    <div>
+      {/* header */}
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Felipe</h2>
+        {/* TEXTO */}
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
         <p>Segunda-feira, 05 de agosto.</p>
 
-        {/* Busca */}
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
-        <Input placeholder="Faça sua busca..." />
-        <Button >
-          <SearchIcon />
-        </Button>
+          <Input />
+          <Search />
         </div>
-        
-      {/* Imagem */}
-      <div className="relative w-full h-[150px] mt-6 rounded-xl">
-        <Image src="/Banner.png" alt="Banner FSW" fill className="object-cover" />
+
+
+        {/* BUSCA RÁPIDA */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button
+              className="gap-2"
+              variant="secondary"
+              key={option.title}
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
+                {option.title}
+              </Link>
+            </Button>
+          ))}
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/BANNER.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        {/* AGENDAMENTO */}
+        <BookingItem />
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
-      
-      {/* Agendamento */}
-
-      <Card className="mt-6">
-        <CardContent className="flex justify-between p-0">
-          {/* Esquerda */}
-          <div className="flex flex-col gap-2 py-5 pl-5">
-            <Badge className="w-fit">Confirmado</Badge>
-            <h3 className="font-semibold">Corte de Cabelo</h3>
-
-            <div className="flex items-center gap-2">
-              <Avatar className="h-6 w-6">
-                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
-              </Avatar>
-              <p className="text-sm">Barbearia FSW</p>
-            </div>
-          </div>
-           {/* Direita */}
-           <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-             <p className="text-sm">Agosto</p>
-             <p className="text-2xl">05</p >
-             <p className="text-sm">20:00</p>
-           </div>
-        </CardContent>
-      </Card>
-
-      </div>
-    </>
-   );
+    </div>
+  )
 }
- 
-export default Home;
+
+export default Home
